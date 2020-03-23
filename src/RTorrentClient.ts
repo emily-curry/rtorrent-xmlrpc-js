a8e67fb5d25d98b5fccd3a9e02ba64b8964b7c52
Emily Curry <emily@curry.sh>
2020-03-22 20:30:24 -0500

    Feat: Expose all generated clients

--- src/RTorrentClient.ts
+++ src/RTorrentClient.ts
@@ -1,6 +1,21 @@
-import { RPCClient, RPCClientOpts } from './util/RPCClient';
+import {
+  DHTClient,
+  DownloadClient,
+  ExecuteClient,
+  FileClient,
+  LoadClient,
+  LogClient,
+  NetworkClient,
+  PeerClient,
+  PiecesClient,
+  ProtocolClient,
+  SessionClient,
+  SystemClient,
+  ThrottleClient,
+  TrackerClient,
+} from './client';
 import { RPCMethodName, RPCMethodParams, RPCMethodReturnType } from './types';
-import { DownloadClient, LoadClient, SystemClient } from './client';
+import { RPCClient, RPCClientOpts } from './util/RPCClient';
 
 export interface RTorrentClientOpts extends RPCClientOpts {}
 
@@ -8,9 +23,20 @@ export class RTorrentClient {
   private rpc: RPCClient;
   constructor(opts: RTorrentClientOpts) {
     this.rpc = new RPCClient(opts);
+    this.dht = new DHTClient(this.rpc);
     this.d = new DownloadClient(this.rpc);
+    this.execute = new ExecuteClient(this.rpc);
+    this.f = new FileClient(this.rpc);
     this.load = new LoadClient(this.rpc);
+    this.log = new LogClient(this.rpc);
+    this.network = new NetworkClient(this.rpc);
+    this.p = new PeerClient(this.rpc);
+    this.pieces = new PiecesClient(this.rpc);
+    this.protocol = new ProtocolClient(this.rpc);
+    this.session = new SessionClient(this.rpc);
     this.system = new SystemClient(this.rpc);
+    this.throttle = new ThrottleClient(this.rpc);
+    this.t = new TrackerClient(this.rpc);
   }
 
   /**
@@ -26,7 +52,18 @@ export class RTorrentClient {
     return await this.rpc.callRPCMethod(methodName, ...params);
   }
 
+  public dht: DHTClient;
   public d: DownloadClient;
+  public execute: ExecuteClient;
+  public f: FileClient;
   public load: LoadClient;
+  public log: LogClient;
+  public network: NetworkClient;
+  public p: PeerClient;
+  public pieces: PiecesClient;
+  public protocol: ProtocolClient;
+  public session: SessionClient;
   public system: SystemClient;
+  public throttle: ThrottleClient;
+  public t: TrackerClient;
 }
