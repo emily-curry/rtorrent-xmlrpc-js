2c338cc84d55a4627f2f1d521561f645d6b65d19
Emily Curry <emily@curry.sh>
2020-04-12 13:03:28 -0500

    Feat: Implement basic connection queuing

--- src/conn/Connection.ts
+++ src/conn/Connection.ts
@@ -1,4 +1,5 @@
 import * as net from 'net';
+import { EventEmitter } from 'events';
 import { SCGIResponseTransformer } from './SCGIResponseTransformer';
 import { XMLDeserializer } from './XMLDeserializer';
 import { serializeMethodCall } from 'xmlrpc/lib/serializer';
@@ -6,10 +7,13 @@ import { serializeMethodCall } from 'xmlrpc/lib/serializer';
 const NULL_CHAR = String.fromCharCode(0);
 
 export class Connection {
+  private readonly clientLockTarget = new EventEmitter();
+  private readonly clientLockQueue: string[] = [];
+  private isProcessingQueue = false;
+
   constructor(private readonly host: string, private readonly port: number) {}
 
   public async rpcMethodCall(method: string, ...params: Array<any>): Promise<unknown> {
-    const client = await this.createClient();
     const xml = serializeMethodCall(method, params);
     const xmlLength = Buffer.byteLength(xml, 'utf8');
 
@@ -17,23 +21,60 @@ export class Connection {
       `CONTENT_LENGTH${NULL_CHAR}${xmlLength}${NULL_CHAR}`,
       `SCGI${NULL_CHAR}1${NULL_CHAR}`,
     ];
-
     const headerLength = scgiHeaderItems.reduce(
       (accumulator, headerItem) => accumulator + headerItem.length,
       0
     );
+    const message = `${headerLength}:${scgiHeaderItems.join('')},${xml}`;
+
+    const client = await this.createClient();
     const des = new XMLDeserializer('UTF-8');
     const response = client.pipe(new SCGIResponseTransformer());
     const result = des.deserializeMethodResponse(response);
-    client.end(`${headerLength}:${scgiHeaderItems.join('')},${xml}`);
-    return (await result) as any; // TODO:
+    client.end(message);
+    return await result;
   }
 
-  private createClient(): Promise<net.Socket> {
-    return new Promise((resolve, reject) => {
-      const client = net.createConnection({ host: this.host, port: this.port }, () => {
-        resolve(client);
-      });
+  private async createClient(): Promise<net.Socket> {
+    return await new Promise((resolve, reject) => {
+      const lockId = this.makeLockId();
+      const onReady = () => {
+        const client = net.createConnection({ host: this.host, port: this.port }, () => {
+          client.on('close', () => this.clientLockTarget.emit(this.releaseEvent(lockId)));
+          resolve(client);
+        });
+      };
+      this.clientLockTarget.once(this.readyEvent(lockId), onReady);
+      this.queueClient(lockId);
     });
+  }
+
+  private queueClient(lockId: string) {
+    this.clientLockQueue.push(lockId);
+    this.processClientLockQueue();
+  }
+
+  private processClientLockQueue() {
+    if (this.isProcessingQueue || this.clientLockQueue.length === 0) return;
+    this.isProcessingQueue = true;
+    const lockId = this.clientLockQueue.shift()!;
+    const onRelease = () => {
+      this.isProcessingQueue = false;
+      this.processClientLockQueue();
+    };
+    this.clientLockTarget.once(this.releaseEvent(lockId), onRelease);
+    this.clientLockTarget.emit(this.readyEvent(lockId));
+  }
+
+  private makeLockId(): string {
+    return [...Array(20)].map((_) => ((Math.random() * 36) | 0).toString(36)).join('');
+  }
+
+  private readyEvent(lockId: string): string {
+    return `ready-${lockId}`;
+  }
+
+  private releaseEvent(lockId: string): string {
+    return `release-${lockId}`;
   }
 }
--- test/client.test.ts
+++ test/client.test.ts
@@ -3,21 +3,36 @@ import { client } from './clientSetup';
 
 describe('client functionality', () => {
   it('connects successfully, but fails with unknown method', async () => {
-    const promise = client.call('not_a_real_method' as any);
+    const promise = client.call('not_a_real_method');
     await expect(promise).rejects.toBeInstanceOf(Error);
     await expect(promise).rejects.toBeInstanceOf(RPCError);
     await expect(promise).rejects.toHaveProperty('faultCode', -506);
   });
 
   it('connects successfully, but fails with no arguments', async () => {
-    const promise = (client.call as any)('d.name');
+    const promise = client.call('d.name');
     await expect(promise).rejects.toBeInstanceOf(RPCError);
     await expect(promise).rejects.toHaveProperty('faultCode', -503);
   });
 
   it('connects successfully, but fails with invalid arguments', async () => {
-    const promise = (client.call as any)('d.name', 'oops', 'not real');
+    const promise = client.call('d.name', 'oops', 'not real');
     await expect(promise).rejects.toBeInstanceOf(RPCError);
     await expect(promise).rejects.toHaveProperty('faultCode', -501);
+  });
+
+  it('can make multiple simultaneous RPC calls', async () => {
+    const calls: Array<Promise<string[]>> = [];
+    for (let i = 0; i < 300; i++) {
+      calls.push(client.system.listMethods());
+    }
+    const promise = Promise.all(calls);
+    await expect(promise).resolves.toHaveLength(calls.length);
+    const results = await promise;
+    expect(results).toBeDefined();
+    const methodsLen = results[0].length;
+    for (const result of results) {
+      expect(result).toHaveLength(methodsLen);
+    }
   });
 });
